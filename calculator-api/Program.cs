e3c2aeb9cb556fadcea21b5c00a8c680d918ab2c
natalia-coelho <nataliacoelho199@gmail.com>
2024-01-29 22:13:49 -0300

    Create component and set up use the Fetch API

--- calculator-api/Program.cs
+++ calculator-api/Program.cs
@@ -7,15 +7,6 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
 builder.Services.AddCors(options => 
 {
     options.AddPolicy("ReactPolicy", builder =>
@@ -25,6 +16,15 @@ builder.Services.AddCors(options =>
                 .AllowAnyMethod();
     });
 });
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
 app.UseCors("ReactPolicy");
 
