7148b08ee55098ac391c551e755ef354ad5b6e76
natalia-coelho <nataliacoelho199@gmail.com>
2024-02-02 21:38:27 -0300

    sample examples test

--- calculator-react/src/App.js
+++ calculator-react/src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
+import React, { useState } from "react";
+import Button from "./Button";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const Calculator = () => {
+      const [result, setResult] = useState(null);
 
-export default App;
+      let url = `http://localhost:5253/api/calculator`;
+
+      const handleOperation = async (operation, num1, num2) => {
+            const response = await fetch(`${url}/${operation}/${num1}/${num2}`);
+            const data = await response.json();
+            setResult(data);
+      };
+
+      return (
+            <div className="App">
+                  <header className="App-header">
+
+                        <div>
+                              <Button label="Add" onClick={ () => handleOperation('Add', 5, 3) } />
+                              <Button label="Subtract" onClick={ () => handleOperation('subtract', 5, 3) } />
+                              <Button label="Multiply" onClick={ () => handleOperation('multiply', 5, 3) } />
+                              <Button label="Divide" onClick={ () => handleOperation('divide', 5, 3) } />
+                        </div>
+                        <div>
+                              Result: { result }
+                        </div>
+                  </header>
+            </div>
+      );
+};
+export default Calculator;
